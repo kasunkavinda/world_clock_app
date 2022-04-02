784128bb65d7f1459ca38be472eac2c08d60ad29
KasunKavindaGeveo <kasunkavinda.bc@gmail.com>
2022-04-02 18:03:42 +0530

    counter

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,8 +6,36 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('init state ran');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('choose location'));
+    print('build state ran');
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        appBar: AppBar(
+          title: Text('Choose a Location'),
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: Column(children: [
+          Text('choose location'),
+          FlatButton(
+              onPressed: () {
+                setState(() {
+                  counter = counter + 1;
+                });
+              },
+              child: Text('Increment'),
+              color: Colors.amber),
+          Text('$counter')
+        ]));
   }
 }
